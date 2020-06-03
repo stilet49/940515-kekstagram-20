49a46f7ffeb8a0c85c7f58285099b9c1b62f0c72
stilet49 <35362145+stilet49@users.noreply.github.com>
2020-06-03 13:08:36 +0300

    Исправляет косячок

--- js/main.js
+++ js/main.js
@@ -47,10 +47,11 @@ function generateRandomComment(comments) {
   var listComments = [];
   var max = comments.length - 1;
   var countComments = getRandomInteger(1, 5);
-  var avatarNumber = getRandomInteger(1, 6);
+
 
   for (var i = 0; i < countComments; i++) {
     var commentNumber = getRandomInteger(0, max);
+    var avatarNumber = getRandomInteger(1, 6);
     listComments.push({
       avatar: generateAvatarURL(avatarNumber),
       message: comments[commentNumber],
