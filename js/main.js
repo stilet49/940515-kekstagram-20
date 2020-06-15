8b119d46859125abf2de308647d9e9e52828d687
stilet49 <35362145+stilet49@users.noreply.github.com>
2020-06-15 17:12:40 +0300

    Добавляет валидацию полей формы загрузки фотографий, а так же применение глубины эффекта при нажатии на пин шкалы глубины эффекта эффекта

--- js/main.js
+++ js/main.js
@@ -200,6 +200,72 @@ var uploadResizeControls = uploadSelectImageForm.querySelector('.img-upload__sca
 var uploadResizeControlsValue = uploadResizeControls.querySelector('.scale__control--value');
 var uploadImagePreview = uploadSelectImageForm.querySelector('.img-upload__preview');
 var effectImagePreview = uploadImagePreview.querySelector('img');
+var uploadFormHashtags = uploadSelectImageForm.querySelector('.text__hashtags');
+var uploadFormDescription = uploadSelectImageForm.querySelector('.text__description');
+
+uploadSelectImageForm.addEventListener('submit', function onUploadSelectImageFormSubmit(evt) {
+  if (!document.activeElement.classList.contains('img-upload__submit')) {
+    evt.preventDefault();
+    return;
+  }
+
+  var isValidForm = validateUploadForm();
+
+  if (!isValidForm) {
+    evt.preventDefault();
+  }
+});
+
+uploadFormDescription.addEventListener('invalid', function (evt) {
+  uploadFormDescription.style.border = '1px solid #f00';
+});
+
+uploadFormDescription.addEventListener('valid', function (evt) {
+  uploadFormDescription.style.border = '';
+});
+
+function resetUploadForm() {
+  uploadFile.value = '';
+  uploadResizeControlsValue.value = '55%';
+  effectImagePreview.className = '';
+  effectImagePreview.style.transform = '';
+
+  uploadEffectControls.querySelector('[name=effect]').checked = true;
+  uploadFormHashtags.value = '';
+  uploadFormDescription.value = '';
+}
+
+function validateUploadForm() {
+  var isValidHashtags = validataFormHashtags();
+
+  if (!isValidHashtags) {
+    uploadFormHashtags.style.border = '1px solid #f00';
+  } else {
+    uploadFormHashtags.style.border = '';
+  }
+
+  return isValidHashtags;
+
+}
+
+function validataFormHashtags() {
+  var hashtags = uploadFormHashtags.value.split(' ').sort();
+  var noMoreThanFiveHashtags = hashtags.length <= 5;
+
+  var hasDuplicate = (hashtags.length === 1) ? false : hashtags.some(function (item, index, array) {
+    if (!array[index + 1]) {
+      return false;
+    }
+
+    return item.toLowerCase() === array[index + 1].toLowerCase();
+  });
+
+  var everyHashtagPasses = hashtags.every(function (item) {
+    return /^#[a-zа-яA-ZА-Я0-9]{1,20}$/.test(item);
+  });
+
+  return noMoreThanFiveHashtags && !hasDuplicate && everyHashtagPasses;
+}
 
 function onUploadOverlayEscPress(evt) {
   if (evt.keyCode !== KEYCODES.Esc) {
@@ -222,6 +288,8 @@ function openUploadOverlay() {
 function closeUploadOverlay() {
   document.removeEventListener('keydown', onUploadOverlayEscPress);
 
+  resetUploadForm();
+
   uploadImage.classList.remove('hidden');
   uploadOverlay.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
@@ -239,34 +307,13 @@ uploadFormCancel.addEventListener('click', function onUploadFormCancelClick(evt)
 
 });
 
-uploadEffectControls.addEventListener('change', function onUploadEffectControlsChange(evt) {
-  if (evt.target.name !== 'effect') {
-    return;
-  }
-
-  var effect = 'effects__preview--' + evt.target.value;
-  var radios = evt.currentTarget.querySelectorAll('[name=effect]');
-
-  radios.forEach(function (item) {
-    var effectName = 'effects__preview--' + item.value;
-
-    if (!effectImagePreview.classList.contains(effectName)) {
-      return;
-    }
-
-    effectImagePreview.classList.remove(effectName);
-  });
-
-  effectImagePreview.classList.add(effect);
-});
-
 uploadResizeControls.addEventListener('click', function onUploadResizeControlsClick(evt) {
   var step = SCALE_STEP;
   var min = SCALE_MIN;
   var max = SCALE_MAX;
 
-  var currentValue = uploadResizeControlsValue.value.slice(0, -1);
-  var newValue = null;
+  var currentValue = parseInt(uploadResizeControlsValue.value.slice(0, -1), 10);
+  var newValue = 0;
 
   if (evt.target.classList.contains('scale__control--smaller')) {
     newValue = currentValue - step;
@@ -279,5 +326,106 @@ uploadResizeControls.addEventListener('click', function onUploadResizeControlsCl
   }
 
   uploadResizeControlsValue.value = newValue + '%';
-  effectImagePreview.style.transform = 'scale(' + (newValue/100) + ')';
+  effectImagePreview.style.transform = 'scale(' + (newValue / 100) + ')';
+});
+
+var EFFECTS = {
+  chrome: {
+    filter: 'grayscale',
+    max: 1
+  },
+  sepia: {
+    filter: 'sepia',
+    max: 1
+  },
+  marvin: {
+    filter: 'invert',
+    max: 100,
+    units: '%'
+  },
+  phobos: {
+    filter: 'blur',
+    max: 3,
+    units: 'px'
+  },
+  heat: {
+    filter: 'brightness',
+    max: 3
+  }
+};
+
+var currentEffect = 'none';
+
+var effectLevel = document.querySelector('.img-upload__effect-level');
+var effectLevelLine = effectLevel.querySelector('.effect-level__line');
+var effectLevelPin = effectLevelLine.querySelector('.effect-level__pin');
+var effectLevelVal = effectLevelLine.querySelector('.effect-level__depth');
+
+function getCoords(elem) {
+  var box = elem.getBoundingClientRect();
+
+  return {
+    left: box.left + window.pageXOffset,
+    top: box.top + window.pageYOffset
+  };
+}
+
+function updateEffectsLevel(percents) {
+  var effect = EFFECTS[currentEffect];
+
+  var value = (typeof percents !== 'undefined') ? (effect.max * percents) / 100 : effect.max;
+
+  value = effect.units ? value + effect.units : value;
+
+  effectImagePreview.style.filter = effect.filter + '(' + value + ')';
+}
+
+function changeLevelDisplay() {
+  if (currentEffect === 'none') {
+    effectLevel.style.display = 'none';
+    effectImagePreview.style.filter = '';
+    return;
+  }
+
+  updateEffectsLevel();
+  effectLevelPin.style.left = '100%';
+  effectLevelVal.style.width = '100%';
+  effectLevel.style.display = 'block';
+}
+
+changeLevelDisplay();
+
+uploadEffectControls.addEventListener('change', function onUploadEffectControlsChange(evt) {
+  if (evt.target.name !== 'effect') {
+    return;
+  }
+
+  effectImagePreview.classList.remove('effects__preview--' + currentEffect);
+
+  effectImagePreview.classList.add('effects__preview--' + evt.target.value);
+
+  currentEffect = evt.target.value;
+
+  changeLevelDisplay();
+});
+
+effectLevelPin.addEventListener('mousedown', function onEffectLeveloinMouseDown(evt) {
+  evt.preventDefault();
+
+  var pinCoords = getCoords(effectLevelPin);
+  var lineCoords = getCoords(effectLevelLine);
+
+  function onEffectLevelPinMouseUp(upEvt) {
+    upEvt.preventDefault();
+
+    var newLeft = pinCoords.left - lineCoords.left + effectLevelPin.offsetWidth * 0.5;
+    var rightEdge = effectLevelLine.offsetWidth;
+    var percents = (newLeft * 100) / rightEdge;
+
+    updateEffectsLevel(percents);
+
+    document.removeEventListener('mouseup', onEffectLevelPinMouseUp);
+  }
+
+  document.addEventListener('mouseup', onEffectLevelPinMouseUp);
 });
