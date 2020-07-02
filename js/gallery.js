4a8dc3860ba00562a4f69f48a8710728158a70b0
stilet49 <35362145+stilet49@users.noreply.github.com>
2020-07-02 10:22:36 +0300

    Добавляет фильтры в галерее

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,78 @@
 'use strict';
 
 (function () {
+  var picturesData = null;
+  var imgFiltersContainer = document.querySelector('.img-filters');
+  var picturesContainer = document.querySelector('.pictures');
+
+  var removePictureElements = function () {
+    var pictureElements = Array.from(picturesContainer.querySelectorAll('.picture'));
+
+    pictureElements.forEach(function (it) {
+      picturesContainer.removeChild(it);
+    });
+  };
+
+  var renderPictures = function (pictures) {
+    var pictureTemplate = document.querySelector('#picture').content;
+
+    var photos = window.picture.addPhotoToPictures(pictures, pictureTemplate);
+
+    picturesContainer.appendChild(photos);
+  };
+
+  var sortFiltersMap = {
+    'filter-default': window.util.debounce(function (sortedArray) {
+      removePictureElements();
+      renderPictures(sortedArray);
+    }),
+
+    'filter-random': window.util.debounce(function (sortedArray) {
+      var sortedByRandomPictures = sortedArray.slice();
+      sortedByRandomPictures = window.util.shuffleArray(sortedByRandomPictures).slice(15);
+
+      removePictureElements();
+      renderPictures(sortedByRandomPictures);
+    }),
+
+    'filter-discussed': window.util.debounce(function (sortedArray) {
+      var sortedByCommentsOrder = sortedArray.slice()
+        .sort(function (a, b) {
+          return b.comments.length - a.comments.length;
+        });
+
+      removePictureElements();
+
+      renderPictures(sortedByCommentsOrder);
+    })
+  };
+
+  var onImgFiltersContainerClick = function (evt) {
+    var target = evt.target;
+    var activeButton = imgFiltersContainer.querySelector('.img-filters__button--active');
+
+    if (target !== activeButton) {
+      if (target.classList.contains('img-filters__button')) {
+        activeButton.classList.remove('img-filters__button--active');
+        target.classList.add('img-filters__button--active');
+        activeButton = target;
+
+        sortFiltersMap[target.id](picturesData);
+      }
+    }
+  };
+
+  var enableSortPictures = function () {
+    imgFiltersContainer.classList.remove('img-filters--inactive');
+    imgFiltersContainer.addEventListener('click', onImgFiltersContainerClick);
+  };
 
   var onSuccess = function (allPictures) {
-    var pictureTemplate = document.querySelector('#picture').content;
-    var pictures = document.querySelector('.pictures');
+    picturesData = allPictures;
 
-    var photos = window.picture.addPhotoToPictures(allPictures, pictureTemplate);
-    pictures.appendChild(photos);
+    renderPictures(allPictures);
+
+    enableSortPictures();
   };
 
   var onError = function (message) {
